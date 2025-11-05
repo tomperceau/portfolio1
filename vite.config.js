b3a59356b18e1b71510e3d8cd833960df4829d79
[tomperceau] <[tom.perceau@etu.unilim.fr]>
2025-11-05 23:02:49 +0100

    la c'est la bonne

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/portfolio1/",
+  build: {
+   rollupOptions: {
+     input: {
+       main: resolve(__dirname, "index.html"),
+       accueil: resolve(__dirname, "pages/contact.html"),
+       plus: resolve(__dirname, "pages/projet.html"),
+       plus: resolve(__dirname, "pages/projet-1.html"),
+       plus: resolve(__dirname, "pages/projet-2.html"),
+       plus: resolve(__dirname, "pages/projet-3.html"),
+       // ajoutez d'autres pages si nécessaire
+     },
+   },
+ },
+
 });
